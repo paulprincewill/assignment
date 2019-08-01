6986cb4f5526dcc45e6b1a77a486c2aee271c0cf
May <mayeloho123@gmail.com>
2019-08-01 13:30:28 +0100

    i updated 'create' frontend

--- ui/create/main.php
+++ ui/create/main.php
@@ -1,3 +1,14 @@
-<h1>
-    Welcome to your new page. Open <b> <?php echo UI.$page_link."/main.php";?> </b> to edit
-</h1>
+<form action="app/create/main" dd_submit="yes">
+    
+    <input type="text" name="title" placeholder="Enter Title Of Assignment">
+    
+     <textarea name="instruction" placeholder="Details of Assignment"></textarea>
+    
+    <label>Assignment Deadline:<input type="date" name="deadline" ></label>
+    
+    
+    <input type="number" name="score" placeholder="Student's Score">
+    
+    <input type="submit">
+    
+</form>
